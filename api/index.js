cd18b45ce9d2de7b0b16f3a7fc44475d3327364d
aziz mobarak <mobarakaziz9@gmail.com>
2020-05-30 07:33:20 +0000

    online email contact api added

--- api/index.js
+++ api/index.js
@@ -3,32 +3,53 @@ const bodyparser = require('body-parser');
 const port = process.env.PORT || 9900;
 const app = express();
 const msql = require('mysql');
-const cors = require('cors')
+const nodemailer = require("nodemailer");
 
+const cors = require('cors')
+app.use(bodyparser.json());
+app.use(bodyparser.text());
 app.use(cors());
 
-const con = msql.createConnection({
-        database: "landing",
-        user: "root",
-        password: '',
-        port: 222
-    })
-    //const cors = require("cors");
-
-app.post('/api', cors(), (req, res) => {
-    var info = req.body;
-    console.log(req.body)
-    if (req.body.text !== null) {
-        con.query("insert into messages (fromemail,subject,message) values ('" + info.from + "','" + info.subject + "','" + info.text + "')",
-            (err, rows, fields) => {
-                if (err) console.log(err)
-                else {
-                    res.send({ status: 'OK' });
-                }
-            }
-        )
-    }
+//const cors = require("cors");
+app.get('/api', (req, res) => {
+    res.send('working here')
 })
+
+app.post('/api', cors(), async(req, res) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "68.65.122.160",
+        port: 26,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'contact@getintohome.store', // generated ethereal user
+            pass: 'xxxxxx', // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: 'contact@getintohome.store', // sender address
+        to: "elgoujilimorad@gmail.com", // list of receivers
+        subject: "from radar page : " + req.body.subject + "", // Subject line
+        text: "from " + req.body.from + "\n" + "Message : " + req.body.text + "", // plain text body
+        //html: "<b>Hello world?</b>", // html body
+    }, );
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+
+    res.send({ status: 'OK' });
+});
+
 
 app.listen(port, () => {
     console.log(port);
